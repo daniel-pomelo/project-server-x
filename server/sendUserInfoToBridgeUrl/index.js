481fb8fa3e660af9cd993b11ccf97f65065a3ed2
Daniel Maldonado <danielmaldonado@Daniels-MacBook-Pro.local>
2022-01-23 14:29:57 -0300

    misma estructura de datos de usuario para casos de registro y al conectarse

--- server/sendUserInfoToBridgeUrl/index.js
+++ server/sendUserInfoToBridgeUrl/index.js
@@ -4,8 +4,17 @@ function sendUserInfoToBridgeUrl(bridgeUrl, user) {
   if (process.env.ENV_NAME === "dev") {
     return Promise.resolve();
   }
+  const userStats = user.stats;
+  const data = {
+    ...user,
+    health: userStats.health,
+    mana: userStats.mana,
+    stats: userStats,
+  };
+  console.log(JSON.stringify(user, null, 2));
+  console.log(JSON.stringify(data, null, 2));
   return axios
-    .post(bridgeUrl, user)
+    .post(bridgeUrl, data)
     .then(function (response) {
       console.log(response);
     })
