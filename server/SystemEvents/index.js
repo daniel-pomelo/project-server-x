9a3ebadf87ae3dcff9d914b34b9e5f2d2d094e59
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-02-03 08:08:20 -0300

    agrego handler de error para la request de sincronizar datos de usuario

--- server/SystemEvents/index.js
+++ server/SystemEvents/index.js
@@ -4,9 +4,19 @@ class SystemEvents {
   static init() {
     return new SystemEvents();
   }
-  notify(user, bridge) {
-    const { url } = bridge;
-    return axios.post(url, user);
+  async notify(user, bridge) {
+    try {
+      const { url } = bridge;
+      await axios.post(url, user);
+    } catch (error) {
+      const { status, config } = error.response;
+      const { method, url, data } = config;
+      console.log("Error trying to sync user data");
+      console.log(status);
+      console.log(method);
+      console.log(url);
+      console.log(JSON.parse(data));
+    }
   }
 }
 
