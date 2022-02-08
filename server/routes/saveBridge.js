1de50b362d08278bf4f13c00fe9b1c6ebd7bb8a5
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-02-08 17:05:35 -0300

    saco logs al guardar el bridge

--- server/routes/saveBridge.js
+++ server/routes/saveBridge.js
@@ -3,8 +3,6 @@ const { saveBridge } = require("../../bridge");
 module.exports = (db) => (req, res) => {
   const bridge_id = req.headers["bridge-id"];
   const { bridge_url } = req.body;
-  console.log("body: ", req.body);
-  console.log("headers: ", req.headers);
   saveBridge(db, bridge_id, bridge_url);
   res.send();
 };
