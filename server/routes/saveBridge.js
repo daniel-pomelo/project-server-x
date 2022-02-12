fca7f8b66f1da8b6975a3b944e97b022a821f219
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-02-12 11:26:32 -0300

    agrego await en save bridge

--- server/routes/saveBridge.js
+++ server/routes/saveBridge.js
@@ -1,8 +1,8 @@
 const { saveBridge } = require("../../bridge");
 
-module.exports = (db) => (req, res) => {
+module.exports = (db) => async (req, res) => {
   const bridge_id = req.headers["bridge-id"];
   const { bridge_url } = req.body;
-  saveBridge(db, bridge_id, bridge_url);
+  await saveBridge(db, bridge_id, bridge_url);
   res.send();
 };
