978edfb0a259b84819793646fd8c4a4cd65c0044
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-02-28 00:26:03 -0300

    env var URL_TO_UI

--- server/routes/getUrlToProfile.js
+++ server/routes/getUrlToProfile.js
@@ -1,7 +1,7 @@
 const getUrlToProfile = (tokens, UI_URL) => (req, res) => {
   const userId = req.params.id;
   const token = tokens.getTokenForProfile(userId);
-  const url = UI_URL + "/profile/" + token;
+  const url = UI_URL + "/auth/" + token;
   res.send({
     url,
   });
