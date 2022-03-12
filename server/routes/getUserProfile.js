5934c9cb866bf5606e114c6422de1769909560c4
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-03-12 13:02:45 -0300

    borro dummy skills

--- server/routes/getUserProfile.js
+++ server/routes/getUserProfile.js
@@ -1,35 +1,11 @@
 const { findUserById } = require("../../user");
 
-const skills = [
-  {
-    id: "heal",
-    name: "Heal",
-    description: "Some description...",
-    level_min: 2,
-    level_gap: 2,
-  },
-  {
-    id: "ignite",
-    name: "Ignite",
-    description: "Some description...",
-    level_min: 2,
-    level_gap: 2,
-  },
-  {
-    id: "chaining",
-    name: "Chaining",
-    description: "Some description...",
-    level_min: 2,
-    level_gap: 3,
-  },
-];
-
 module.exports = (db, tokens) => async (req, res) => {
   const token = req.params.token;
   const userId = tokens.getUserIdFromToken(token);
   const user = await findUserById(db, userId);
 
-  const moreSkills = await db.find("Skills");
+  const skills = await db.find("Skills");
 
-  res.send({ ...user, skills: [...skills, ...moreSkills] });
+  res.send({ ...user, skills });
 };
