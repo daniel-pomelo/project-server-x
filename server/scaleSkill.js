eecf26f3d634641ad136b625c9c4ebe3274d3e1c
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-04-04 19:48:11 -0300

    agrego escalado de propiedades de skill

--- server/scaleSkill.js
+++ server/scaleSkill.js
@@ -1,5 +1,7 @@
 function incrementBySkillLevel(prop_base, skill_level_value) {
-  return prop_base + (prop_base / 100) * 20 * (skill_level_value - 1);
+  return (
+    prop_base + Math.floor((prop_base / 100) * 20) * (skill_level_value - 1)
+  );
 }
 
 function scaleSkill(skill) {
@@ -12,13 +14,30 @@ function scaleSkill(skill) {
         ),
       }
     : {};
+  const health_other = skill.health_other
+    ? {
+        health_other: incrementBySkillLevel(
+          skill.health_other,
+          skill_level_value
+        ),
+      }
+    : {};
   const mana_self = skill.mana_self
     ? { mana_self: incrementBySkillLevel(skill.mana_self, skill_level_value) }
+    : {};
+  const mana_other = skill.mana_other
+    ? { mana_other: incrementBySkillLevel(skill.mana_other, skill_level_value) }
+    : {};
+  const amount = skill.amount
+    ? { amount: incrementBySkillLevel(skill.amount, skill_level_value) }
     : {};
   return {
     ...skill,
     ...health_self,
+    ...health_other,
     ...mana_self,
+    ...mana_other,
+    ...amount,
   };
 }
 
