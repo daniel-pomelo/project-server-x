544dfa2c4d38d0d842495c41c9e58ebcb365afeb
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-07-24 13:22:13 -0300

    revuelvo 404 si el usuario no es encontrado

--- server/routes/getUserProfile.js
+++ server/routes/getUserProfile.js
@@ -5,10 +5,19 @@ module.exports = (db) => async (req, res) => {
   const token = req.params.token;
   const userId = await getUserIdFromToken(token);
   const user = await findUserById(db, userId);
-  const skillsCatalog = await db.find("Skills");
+  const [skillsCatalog, userMeter = {}] = await Promise.all([
+    db.find("Skills"),
+    db.findOne("UserMeters", { user_id: userId }),
+  ]);
 
+  if (!user) {
+    return res.status(404).send({});
+  }
   res.send({
     ...user,
+    meter: {
+      status: userMeter.status,
+    },
     skills_catalog: skillsCatalog,
   });
 };
