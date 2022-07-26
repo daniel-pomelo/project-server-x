7bf766364c277e13e4fe0977a838d01aa26e3442
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-07-25 22:36:37 -0300

    fix: cuando user-meter es nulo

--- server/routes/getUserProfile.js
+++ server/routes/getUserProfile.js
@@ -5,7 +5,7 @@ module.exports = (db) => async (req, res) => {
   const token = req.params.token;
   const userId = await getUserIdFromToken(token);
   const user = await findUserById(db, userId);
-  const [skillsCatalog, userMeter = {}] = await Promise.all([
+  const [skillsCatalog, userMeter] = await Promise.all([
     db.find("Skills"),
     db.findOne("UserMeters", { user_id: userId }),
   ]);
@@ -13,10 +13,11 @@ module.exports = (db) => async (req, res) => {
   if (!user) {
     return res.status(404).send({});
   }
+  const meterStatus = userMeter ? userMeter.status : "pending";
   res.send({
     ...user,
     meter: {
-      status: userMeter.status,
+      status: meterStatus,
     },
     skills_catalog: skillsCatalog,
   });
