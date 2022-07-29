e2a19ddf928eda4ed0d478338f01d17b2cc34cb0
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-07-28 22:01:55 -0300

    agrego descripcion a endpoint

--- server/routes/saveBridge.js
+++ server/routes/saveBridge.js
@@ -1,5 +1,10 @@
 const { saveBridge } = require("../../bridge");
 
+/**
+ * Fired whenever bridge's url changes and we need to save it
+ * @param {*} bridge_url
+ * @returns
+ */
 module.exports = (db) => async (req, res) => {
   const bridge_id = req.headers["bridge-id"];
   const { bridge_url } = req.body;
