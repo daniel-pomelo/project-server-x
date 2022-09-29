533e12606bea4603cc8e6f712938b7a83267d0c3
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-09-28 21:56:16 -0300

    agrego algo de doc en clean-clanes

--- scripts/clean-clans.js
+++ scripts/clean-clans.js
@@ -1,6 +1,27 @@
 const MongoDataBase = require("../MongoDataBase");
 const deleteAll = require("./deleteAll");
 
+// <Clans>
+// name: string, description: string, created_at: timestamp, disabled: boolean
+// -> Contains clan data.
+//
+// <UserClans>
+// user_id: id, clan_id: id
+// -> Relationship 1:1 between a clan an his admin.
+//
+// <UserClanInvitations>
+// invitado_id: id, invitador_id: id, clan_id: id,
+// status: string, timestamp: timestamp, updated_at: timestamp
+// -> Represent an invitation between a clan admin to join.
+//
+// <UserClanMembers>
+// member_id: id, clan_id: id, timestamp: timestamp
+// status: string, brokeup_at: timestamp
+//
+// <ClanRelationships>
+//
+//
+
 MongoDataBase.init({ database: "ProjectX" }).then(async (db) => {
   await deleteAll(db, "ClanRelationships");
   await deleteAll(db, "Clans");
