9bbf4d5c958a44e32edf6131e5d68d39eae8f82b
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-10-18 06:58:18 -0300

    agrego endpoint de crear conquer point

--- server/conquer/index.js
+++ server/conquer/index.js
@@ -3,45 +3,74 @@
 //  - clan_id
 //  - disputers
 
+const uuid = require("uuid");
 const { timestamp } = require("../../time");
+const assertBridgeIsEnabled = require("../routes/assertBridgeIsEnabled");
 const asyncHandler = require("../routes/asyncHandler");
 
 module.exports = {
   setPointAsConquered: (app, db) => {
     app.post(
       "/api/conquer",
-      asyncHandler(async (req, res) => {
-        assertHasRequiredParameters(req);
-        const [clan, conquest] = await Promise.all([
-          getClan(req, db),
-          getConquest(req, db),
-        ]);
-        await db.save("Conquests", {
-          ...req.body,
-          timestamp: timestamp(),
-        });
-        res.send({
-          conquest,
-          clan,
-        });
-      })
+      asyncHandler(assertBridgeIsEnabled(db)),
+      assertHasRequiredParameters,
+      asyncHandler(setPointAsConquered(db))
     );
+  },
+  createPoint: (app, db) => {
+    app.post("/api/conquer-point", asyncHandler(createConquerPoint(db)));
   },
 };
 
-async function getConquest(req, db) {
-  const conquest = await db.findOne("Conquests", { id: req.body.conquer_id });
-  if (!conquest) {
+async function createConquerPoint(req, res) {
+  await db.save("ConquestPoints", {
+    id: uuid.v4(),
+    status: "active",
+    ttl: 3600,
+    timestamp: timestamp(),
+  });
+  res.send({});
+}
+
+function setPointAsConquered(db) {
+  return async (req, res) => {
+    const [clan, conquestPoint] = await Promise.all([
+      getClan(req, db),
+      getConquestPoint(req, db),
+    ]);
+    await db.save("Conquests", {
+      ...req.body,
+      timestamp: timestamp(),
+    });
+    res.send({
+      point: conquestPoint,
+      clan,
+    });
+  };
+}
+
+async function getConquestPoint(req, db) {
+  //ConquestPoints
+  // status (initial state: 'active', also posibles: conquered, disabled)
+  // created_at
+  // -> status = 'conquered' => conquered_at
+  // -> status = 'disabled' => disabled_at
+  // ttl (unit: seconds)
+  //
+  const conquestPoint = await db.findOne("ConquestPoints", {
+    id: req.body.conquer_id,
+  });
+  if (!conquestPoint) {
     const e = new Error("BAD_REQUEST");
     e.context = "CLAN_CONQUERING_POINT";
-    e.reason = "Conquest not found.";
+    e.reason = "Conquest point not found.";
     e.payload = {
       body: req.body,
       headers: req.headers,
     };
     throw e;
   }
-  return conquest;
+  return conquestPoint;
 }
 
 async function getClan(req, db) {
@@ -61,7 +90,7 @@ async function getClan(req, db) {
   return clan;
 }
 
-function assertHasRequiredParameters(req) {
+function assertHasRequiredParameters(req, res, next) {
   if (!req.body.clan_id) {
     const e = new Error("BAD_REQUEST");
     e.context = "CLAN_CONQUERING_POINT";
@@ -82,4 +111,18 @@ function assertHasRequiredParameters(req) {
     };
     throw e;
   }
+  if (
+    !Array.isArray(req.body.disputers) ||
+    (req.body.disputers && req.body.disputers.length === 0)
+  ) {
+    const e = new Error("BAD_REQUEST");
+    e.context = "CLAN_CONQUERING_POINT";
+    e.reason = "Missing disputers.";
+    e.payload = {
+      body: req.body,
+      headers: req.headers,
+    };
+    throw e;
+  }
+  next();
 }
