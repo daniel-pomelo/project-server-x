846c6b0c16fbd138a3f7943909bf6170d79813e2
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-10-21 21:47:11 -0300

    fix en log de la data que envia al bridge para spawmear el conquest point

--- server/conquer/launchConquestPoint.js
+++ server/conquer/launchConquestPoint.js
@@ -35,12 +35,10 @@ function launchConquestPoint(db) {
       launched_at: timestamp(),
     });
 
-    console.log(requestResult);
-
     await db.save("ConquestPointLaunchings", {
       body: req.body,
-      request_body: requestResult.body,
-      request_result: requestResult.data,
+      request_body: requestResult?.body,
+      request_result: requestResult?.config?.data,
       request_result_status: requestResult.status,
       conquer_point: conquestPoint,
       bridge,
