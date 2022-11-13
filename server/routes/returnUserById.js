2969942b50bc3c0d9ca9cc2451af202782943ad7
daniel-pomelo <daniel.maldonado@pomelo.la>
2022-11-12 22:53:39 -0300

    borro archivo innecesario

--- server/routes/returnUserById.js
+++ server/routes/returnUserById.js
@@ -1,11 +1,24 @@
-const getUser = require("./getUser");
+const { findUserById } = require("../../user");
 
-const returnUserById = (db) => async (req, res, next) => {
-  try {
-    const userId = req.params.id;
-    await getUser(db)(req, res);
-  } catch (error) {
-    next(error);
+const returnUserById = (db) => async (req, res) => {
+  const userId = req.params.id;
+  const user = await findUserById(db, userId);
+
+  if (user) {
+    if (user.isDisabled) {
+      return res.status(403).send({
+        user_id: userId,
+      });
+    }
+    res.send({
+      ...user,
+      skills: [],
+      skills_url: `${process.env.BACKEND_URL}/api/skills/${userId}`,
+    });
+  } else {
+    res.status(404).send({
+      user_id: userId,
+    });
   }
 };
 
